e5a88f65da0a9883d1f49be0b92f3253fdd47437
hsimingLiu <liuseemin@gmail.com>
2014-10-18 12:02:18 +0800

    using openshift internal ip and port

--- server.js
+++ server.js
@@ -36,7 +36,10 @@ var server = http.createServer(function(request, response) {
 	}
 });
 
-server.listen(8080);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+server.listen(server_port, server_ip_address);
 
 var serv_io = io.listen(server);
 console.log('server listening...')
